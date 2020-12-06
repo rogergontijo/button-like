48e19b3f159083c05b75b66affe4ff2390522118
Roger <gontijo.roger@gmail.com>
2020-12-06 01:48:58 -0300

    Add component LikeButton;

--- front/src/components/LikeButton.js
+++ front/src/components/LikeButton.js
@@ -2,35 +2,67 @@ import React from "react";
 
 export default class LikeButton extends React.Component {
   state = {
-    liked: true,
-    article: null    
+    liked: false,
+    article: null,
+    count: 0    
+  };
+
+   Increment = async () => {
+    const articleGuid = "D6283F6D-3465-4D37-8C01-8DC4B85AC95D";
+    let amountLike = 0; 
+    if (this.state.liked)
+    {    
+        amountLike = -1;
+        this.setState({liked: false});
+    }
+    else
+    {
+        amountLike = 1;
+        this.setState({liked: true});
+    }
+
+    const url = "https://localhost:44323/articles/";
+    await fetch(url, {
+                method: "put",
+                headers: { "Content-type": "application/json" },
+                body: JSON.stringify({
+                    Guid: articleGuid,
+                    AmountLike: amountLike
+                })
+            })
+            .then(async () => {
+                const response = await fetch(url + articleGuid)
+                const data = await response.json();                
+                    console.log(data);
+                    this.setState({ 
+                        article: data, 
+                        liked: this.state.liked, 
+                        count: data.amountLike 
+                    });                    
+            })
+            .catch(e => console.error(e));    
   };
 
   async componentDidMount() {
-    const articleGuid = "";
+    const articleGuid = "D6283F6D-3465-4D37-8C01-8DC4B85AC95D";
     const url = "https://localhost:44323/articles/" + articleGuid;
     const response = await fetch(url);
-    const data = await response.json();
-    this.setState({ article: data.results[0], liked: true });
-  }
+    const data = await response.json();    
+    this.setState({ article: data, liked: this.state.liked, count: data.amountLike });    
+  };  
 
   render() {
-    if (this.state.liked) {
-      return <div>loading...</div>;
-    }
-
     if (!this.state.article) {
       return <div>didn't get a article</div>;
     }
 
     return (
-      <div>
-        <div>{this.state.article.name}</div>
-        <div>{this.state.article.text}</div>
-        <div>{this.state.article.authorGuid}</div>        
-        <div>{this.state.article.amountLike}</div>
+      <div>        
+        <div><h4>Article Name</h4> {this.state.article.name}</div>
+        <div><h4>Article Text:</h4> {this.state.article.text}</div>
+        <div><h4>Author Guid:</h4> {this.state.article.authorGuid}</div>
+        <button onClick={this.Increment}>Likes: {this.state.count}</button>         
       </div>
     );
   }
-
 }
